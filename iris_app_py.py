9ac3d4cfa8eca8ca6714f75707ff74892ac94dd3
dinashmaliyasolutionIT <maliyadinash@gmail.com>
2025-11-07 11:28:33 +0500

    Fix pip install command syntax

--- iris_app_py.py
+++ iris_app_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1opCSlQi0uDcoVhIFJdtVn1a580plmZmp
 """
 
-!pip install streamlit
+pip install streamlit
 import streamlit as st
 import pickle
 import numpy as np
